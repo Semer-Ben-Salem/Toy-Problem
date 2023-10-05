fdf0fef554ff07f0a910a56da1e29eeae6571ac7
Semer <semer_ben_salem@yahoo.fr>
2023-10-05 23:44:30 +0100

    Return Something to Me!

---
+++ Return Something to Me!/script.js
@@ -0,0 +1,17 @@
+/*
+Write a function that returns the string "something" joined with a space " " and the given argument a.
+Examples
+
+giveMeSomething("is better than nothing") ➞ "something is better than nothing"
+
+giveMeSomething("Bob Jane") ➞ "something Bob Jane"
+
+giveMeSomething("something") ➞ "something something"
+*/
+
+function giveMeSomething(a) {
+  if (typeof a === 'string') {
+    return `something ${a}`;
+  }
+  return 'please provide a string';
+}
