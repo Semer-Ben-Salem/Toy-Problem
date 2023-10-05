99179ecc806492cbdda8a2dddd84eb125ee71eef
Semer <semer_ben_salem@yahoo.fr>
2023-10-05 23:35:49 +0100

    Return the Next Number from the Integer Passed

---
+++ Return the Next Number from the Integer Passed/script.js
@@ -0,0 +1,16 @@
+/*
+Create a function that takes a number as an argument, increments the number by +1 and returns the result.
+Examples
+
+addition(0) ➞ 1
+
+addition(9) ➞ 10
+
+addition(-3) ➞ -2
+*/
+function addition(num) {
+  if (Number.isInteger(num)) {
+    return num + 1;
+  }
+  return ' please return an integer';
+}
