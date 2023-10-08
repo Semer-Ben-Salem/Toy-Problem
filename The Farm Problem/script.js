0058b6e2c7c1dde856ddacddb7e131de559659a0
Semer <semer_ben_salem@yahoo.fr>
2023-10-08 13:21:48 +0100

    The Farm Problem

---
+++ The Farm Problem/script.js
@@ -0,0 +1,28 @@
+/*
+In this challenge, a farmer is asking you to tell him how many legs can be counted among all his animals. The farmer breeds three species:
+
+    chickens = 2 legs
+    cows = 4 legs
+    pigs = 4 legs
+
+The farmer has counted his animals and he gives you a subtotal for each species. You have to implement a function that returns the total number of legs of all the animals.
+Examples
+
+animals(2, 3, 5) ➞ 36
+
+animals(1, 2, 3) ➞ 22
+
+animals(5, 2, 8) ➞ 50
+*/
+
+function animals(chickens, cows, pigs) {
+  if (
+    Number.isInteger(chickens + cows + pigs) &&
+    chickens >= 0 &&
+    cows >= 0 &&
+    pigs >= 0
+  ) {
+    return `number of legds: ${cows * 4 + chickens * 2 + pigs * 4}`;
+  }
+  return 'please provide the 3 integers greater or equal to 0';
+}
